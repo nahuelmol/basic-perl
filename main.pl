fba73fd15f5fd80dc227b892b00dd3ab5663417f
Silvio Molina <molinahuel44@gmail.com>
2021-05-07 02:15:35 -0300

    second commit

--- main.pl
+++ main.pl
@@ -4,9 +4,19 @@ package main;
 use DBI;
 use strict;
 use warnings;
+use LWP::Simple;
 
-my $myConnection = DBI->connect("DBI:Pg:dbname=postdb;host=localhost","postgres");
-my $query = $myConnection->prepare("CREATE TABLE mocos (name VARCHAR(50));");
-my $result = $query->execute();
+sub make_req {
+	my $contents = get("https://www.google.com");
+	return $contents;
+}
 
-print("result");
+sub make_db_conn {
+	my $myConnection = DBI->connect("DBI:Pg:dbname=postdb;host=localhost","postgres");
+	my $query = $myConnection->prepare("CREATE TABLE mocos (name VARCHAR(50));");
+	my $result = $query->execute();
+}
+
+my $result =make_req();
+
+print($result);
